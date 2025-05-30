5f7d6c9446719a5fa3912789e40bfbb6935407a2
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-05-30 17:57:57 +0530

    working turn

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -292,7 +292,7 @@ const createPeerConnection = (remoteUserId) => {
   // send our ICE
   pc.onicecandidate = e => {
     if (e.candidate) {
-      console.log("ICE candidate:", e.candidate.candidate);
+      // console.log("ICE candidate:", e.candidate.candidate);
       socketRef.current.emit("ice-candidate", { candidate: e.candidate, to: remoteUserId });
     }
   };
