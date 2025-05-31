6023add1d7698438bf090046e2af011ea47030c8
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2025-06-01 02:02:03 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,18 @@ const io = socketIo(server, {
   }
 });
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://focalpoint-gamma.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 // Middleware
 app.use(cors({
   origin: [
